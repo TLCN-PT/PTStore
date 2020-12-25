0ceabee9e29e248d7d76ba628e71bb01e1c42925
phungnm99-it <62021483+phungnm99-it@users.noreply.github.com>
2020-12-25 22:45:34 +0700

    add View DienThoai and sort function

---
+++ PTStore/Common/ViewModels/DienThoaiViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PTStore.Models;
+
+namespace PTStore.Common.ViewModels
+{
+    public class DienThoaiViewModel
+    {
+        public List<ThuongHieu> lstThuongHieu { get; set; }
+        public List<DienThoai> lstDienThoai { get; set; }
+    }
+}
